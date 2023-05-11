55135323035135aa0b264869045d1c196345fdc9
user <user@526-10>
2023-05-11 11:16:09 +0400

    Добавьте файлы проекта.

---
+++ WpfApp12/Models/Car.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp12.Models
+{
+    public class Car
+    {
+
+        public string Marka {get; set; }
+        public string Color { get; set; }
+    }
+}
---
+++ WpfApp12/ViewModel/MainViewModel.cs
@@ -0,0 +1,36 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp12.Models;
+
+namespace WpfApp12.ViewModel
+{
+    internal partial class MAinViewModel : ObservableObject
+    {
+
+        [ObservableProperty] private Car selectedCar;
+
+
+        [RelayCommand] 
+        private void GenerateRandomCar()
+        {
+            string[] marks = { "kia", "lada", "toyota" };
+            string[] colors = { "red", "blue", "purple" };
+
+
+            Car car = new Car()
+            {
+                Color = colors[new Random().Next(0, colors.Length)],
+                Marka = marks[new Random().Next(0, marks.Length)]
+            };
+
+            SelectedCar = car;
+
+        }
+
+    }
+}
